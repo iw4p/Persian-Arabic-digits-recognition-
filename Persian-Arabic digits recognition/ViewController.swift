e20d9ecd98242a5e410dcc2c2703e7887cce5dfd
Nima Akbarzade <Nimakbarzade@yahoo.com>
2019-03-08 22:17:46 +0330

    first commit

---
+++ Persian-Arabic digits recognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Persian-Arabic digits recognition
+//
+//  Created by Nima Akbarzade on 12/17/1397 AP.
+//  Copyright © 1397 AP Nima Akbarzade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
